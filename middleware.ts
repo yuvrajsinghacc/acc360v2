33b5ad1bc63ee414b59560cbc7e1cd5a4d8bc750
Yuvraj Singh <yuvrajsinghacademic@gmail.com>
2026-06-23 13:41:55 -0700

    fix: exempt /api/newsletter/generate from Clerk auth (cron route)

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/api/newsletter/generate',
+])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
